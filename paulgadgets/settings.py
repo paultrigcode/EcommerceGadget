3c6927aa9f3bc891eb7e7eff06bdfca09835aa73
Ajakaye <mailpaulade@gmail.com>
2021-02-16 06:57:12 +0100

    modified settings

--- paulgadgets/settings.py
+++ paulgadgets/settings.py
@@ -191,7 +191,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
